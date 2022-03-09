4ca768bc6fd22bef2acd652804890f80a9aa20ca
Bruno Lima <brunomlima23@gmail.com>
2022-03-09 12:53:01 -0300

    first commit

---
+++ SalesWebMvc2022/Data/SalesWebMvc2022Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc2022.Models;
+
+namespace SalesWebMvc2022.Data
+{
+    public class SalesWebMvc2022Context : DbContext
+    {
+        public SalesWebMvc2022Context (DbContextOptions<SalesWebMvc2022Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvc2022.Models.Department> Department { get; set; }
+    }
+}
---
+++ SalesWebMvc2022/Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesWebMvc2022.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
